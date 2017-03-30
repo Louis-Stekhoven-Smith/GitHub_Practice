0780c56eec426e8dbabfdb2b27139b863ee93ebb
Louis <louis.stekhoven@gmail.com>
2017-03-30 14:21:56 +1100

    extracted hello word

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        sayHello();
+    }
+
+
+    private static void sayHello(){
         System.out.println("Hello World!");
     }
 }
